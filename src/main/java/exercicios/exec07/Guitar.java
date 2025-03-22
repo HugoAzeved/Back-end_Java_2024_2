7481132895dd72e79fcb1a9d538580932cf3350f
Hugo <hugoazevedo649@gmail.com>
2025-03-22 17:30:16 -0300

    exercicios

--- src/main/java/exercicios/exec07/Guitar.java
+++ src/main/java/exercicios/exec07/Guitar.java
@@ -3,6 +3,6 @@ package exercicios.exec07;
 public class Guitar implements Playable{
     @Override
     public void play (){
-        System.out.printf("Som de Guitarra!!" + "\n");
+        System.out.println("Som de Guitarra!!" + "\n");
     }
 }
